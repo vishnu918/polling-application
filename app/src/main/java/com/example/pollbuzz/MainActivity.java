94cb5d901b802f41265315fba09149716c3b2fde
26112000 <madhurakunjir2611@gmail.com>
2020-03-21 14:59:09 +0530

    FirstCommit

--- app/src/main/java/com/example/pollbuzz/MainActivity.java
+++ app/src/main/java/com/example/pollbuzz/MainActivity.java
@@ -4,19 +4,30 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
+import com.google.android.material.button.MaterialButton;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
     FirebaseAuth auth;
+    MaterialButton button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         auth=FirebaseAuth.getInstance();
+        button = findViewById(R.id.click);
 
 
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i=new Intent(MainActivity.this,Poll_list.class);
+                startActivity(i);
+            }
+        });
     }
 
     @Override
@@ -28,4 +39,7 @@ public class MainActivity extends AppCompatActivity {
             startActivity(i);
         }
     }
+
+
+
 }
