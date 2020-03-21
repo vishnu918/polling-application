c74da49256878a05babbaa5565a7bf838ec7632c
medhavi <medhavi.srivastava16@gmail.com>
2020-03-21 13:42:16 +0530

    Set option name feature added

--- app/src/main/java/com/example/pollbuzz/Single_type_poll.java
+++ app/src/main/java/com/example/pollbuzz/Single_type_poll.java
@@ -3,15 +3,25 @@ package com.example.pollbuzz;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.app.Dialog;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.Toast;
+
+import com.google.android.material.textfield.TextInputLayout;
 
 public class Single_type_poll extends AppCompatActivity {
     Button add;
     RadioGroup group;
+    String name;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,13 +42,53 @@ public class Single_type_poll extends AppCompatActivity {
                 RadioButton button=new RadioButton(getApplicationContext());
                 button.setLayoutParams(new RadioGroup.LayoutParams(RadioGroup.LayoutParams.MATCH_PARENT,RadioGroup.LayoutParams.WRAP_CONTENT));
                 String t="Option"+(group.getChildCount()+1);
-                button.setText(t);
+                //
+               showDialog(Single_type_poll.this, button);
+
                 button.setTag(t.toLowerCase());
+                group.removeAllViews();
+                //button.setText(name);
                 group.addView(button);
                 group.setVisibility(View.VISIBLE);
 
             }
         });
+
+
+    }
+    public void showDialog(Activity activity, final RadioButton button){
+        final Dialog dialog = new Dialog(activity);
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        dialog.setContentView(R.layout.set_name_dialog);
+        WindowManager.LayoutParams lp = new WindowManager.LayoutParams();
+        Window window = dialog.getWindow();
+        lp.copyFrom(window.getAttributes());
+        lp.width = WindowManager.LayoutParams.MATCH_PARENT;
+        lp.height = WindowManager.LayoutParams.WRAP_CONTENT;
+
+
+        dialog.setCancelable(false);
+        dialog.show();
+        window.setAttributes(lp);
+
+
+
+        final TextInputLayout text =  dialog.findViewById(R.id.name);
+
+
+        Button dialogButton = (Button) dialog.findViewById(R.id.done);
+        dialogButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                name=text.getEditText().getText().toString();
+                button.setText(name);
+                Toast.makeText(getApplicationContext(),name,Toast.LENGTH_LONG).show();
+                dialog.dismiss();
+
+            }
+        });
+
+
 
     }
 }
