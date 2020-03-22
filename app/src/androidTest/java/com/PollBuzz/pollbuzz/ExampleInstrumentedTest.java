900596bced05a93aa8cd5d0bad501d08506faa07
TriceriaDivyank <62383560+TriceriaDivyank@users.noreply.github.com>
2020-03-22 12:31:54 +0530

    Revert "Mcq multiple Poll"

--- app/src/androidTest/java/com/PollBuzz/pollbuzz/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/PollBuzz/pollbuzz/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package com.PollBuzz.pollbuzz;
+package com.example.pollbuzz;
 
 import android.content.Context;
 
@@ -22,6 +22,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
 
-        assertEquals("com.PollBuzz.pollbuzz", appContext.getPackageName());
+        assertEquals("com.example.pollbuzz", appContext.getPackageName());
     }
 }
