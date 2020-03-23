e78f28520d8ff746b2af587ca0177f73c23b36ea
medhavi <medhavi.srivastava16@gmail.com>
2020-03-24 02:51:22 +0530

    [Medhavi] | Single_Type_Response: Added logout and going to homepage functionality

--- app/src/main/java/com/PollBuzz/pollbuzz/Single_type_response.java
+++ app/src/main/java/com/PollBuzz/pollbuzz/Single_type_response.java
@@ -6,19 +6,28 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.res.ResourcesCompat;
 
 import android.app.Dialog;
+import android.content.Intent;
+import android.graphics.Paint;
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ProgressBar;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
@@ -35,11 +44,19 @@ public class Single_type_response extends AppCompatActivity {
     TextView title, query;
     RadioGroup group;
     FirebaseFirestore db;
+    CollectionReference ref;
     Map<String,Integer> options;
     String key;
     Typeface typeface;
      Dialog dialog;
-
+    FirebaseAuth auth;
+    ImageButton home,logout;
+    FirebaseAuth.AuthStateListener listener;
+    Button submit;
+    int c;
+    Map<String,Integer> response;
+    int b_id;
+    String resp;
 
 
     @Override
@@ -50,15 +67,46 @@ public class Single_type_response extends AppCompatActivity {
         getSupportActionBar().setDisplayShowCustomEnabled(true);
         getSupportActionBar().setCustomView(R.layout.action_bar);
         View view =getSupportActionBar().getCustomView();
+        home = view.findViewById(R.id.home);
+        logout = view.findViewById(R.id.logout);
+        c=0;
+        home.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(Single_type_response.this, MainActivity.class);
+                startActivity(i);
+            }
+        });
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                auth.signOut();
+            }
+        });
         title=findViewById(R.id.title);
+        submit=findViewById(R.id.submit);
         query=findViewById(R.id.query);
         group=findViewById(R.id.options);
         db=FirebaseFirestore.getInstance();
        options=new HashMap<>();
+       response=new HashMap<>();
        key= "HuIbrwZXR6piG0uSBWWc";
        typeface= ResourcesCompat.getFont(getApplicationContext(),R.font.didact_gothic);
        dialog=new Dialog(Single_type_response.this);
         showDialog();
+        auth = FirebaseAuth.getInstance();
+        listener=new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user=firebaseAuth.getCurrentUser();
+                if(user==null)
+                {
+                    Intent i=new Intent(Single_type_response.this, Login_Signup_Activity.class);
+                    startActivity(i);
+                }
+
+            }
+        };
        db.collection("Polls").document(key).get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
            @Override
            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
@@ -73,9 +121,10 @@ public class Single_type_response extends AppCompatActivity {
                    dialog.dismiss();
                        Polldetails polldetails=data.toObject(Polldetails.class);
                        title.setText(polldetails.getTitle());
+                       title.setPaintFlags(title.getPaintFlags()| Paint.UNDERLINE_TEXT_FLAG);
                        query.setText(polldetails.getQuestion());
                        options=polldetails.getMap();
-                       int c=options.size();
+
                        for(Map.Entry<String,Integer> entry : options.entrySet())
                        {
                            RadioButton button=new RadioButton(getApplicationContext());
@@ -83,9 +132,9 @@ public class Single_type_response extends AppCompatActivity {
                            layoutParams.setMargins(5,20,5,20);
                            button.setLayoutParams(layoutParams);
                            button.setTypeface(typeface);
+                           /*button.setId(c+1);*/
                            button.setText(entry.getKey());
                            button.setTextSize(20.0f);
-
                            group.addView(button);
                        }
                    }
@@ -93,6 +142,7 @@ public class Single_type_response extends AppCompatActivity {
 
                }
            }
+
        );
 
 
@@ -113,5 +163,11 @@ public class Single_type_response extends AppCompatActivity {
         dialog.setCancelable(false);
         dialog.show();
         window.setAttributes(lp);
+    }
+    @Override
+    protected void onStart() {
+        super.onStart();
+        auth.addAuthStateListener(listener);
+
     }
 }
