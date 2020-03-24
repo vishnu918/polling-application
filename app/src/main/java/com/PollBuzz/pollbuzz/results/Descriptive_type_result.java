34ca158c422a1b5c07a696228cf2aa367b239945
medhavi <medhavi.srivastava16@gmail.com>
2020-03-25 02:45:36 +0530

    [Medhavi] | Descriptive_Type_Response: Added activity,layout and action bar functionality

---
+++ app/src/main/java/com/PollBuzz/pollbuzz/results/Descriptive_type_result.java
@@ -0,0 +1,95 @@
+package com.PollBuzz.pollbuzz.results;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.res.ResourcesCompat;
+
+import android.app.Dialog;
+import android.content.Intent;
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+import com.PollBuzz.pollbuzz.LogIn_SignUp.Login_Signup_Activity;
+import com.PollBuzz.pollbuzz.MainActivity;
+import com.PollBuzz.pollbuzz.R;
+import com.PollBuzz.pollbuzz.responses.Descriptive_type_response;
+import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Descriptive_type_result extends AppCompatActivity {
+    Button submit;
+    TextView title,query;
+    Map<String,Integer> response;
+    Typeface typeface;
+    Dialog dialog;
+    FirebaseAuth auth;
+    ImageButton home,logout;
+    FirebaseAuth.AuthStateListener listener;
+    FirebaseFirestore db;
+    CollectionReference ref;
+    TextInputLayout answer;
+    String key,uid;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_descriptive_type_result);
+        getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
+        getSupportActionBar().setDisplayShowCustomEnabled(true);
+        getSupportActionBar().setCustomView(R.layout.action_bar);
+        View view =getSupportActionBar().getCustomView();
+        home = view.findViewById(R.id.home);
+        logout = view.findViewById(R.id.logout);
+        home.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(Descriptive_type_result.this, MainActivity.class);
+                startActivity(i);
+            }
+        });
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                auth.signOut();
+            }
+        });
+        title=findViewById(R.id.title);
+        query=findViewById(R.id.query);
+        db=FirebaseFirestore.getInstance();
+        response=new HashMap<>();
+        key= "IydixH3XGmrNDnHwxTC7";
+        uid="3fpFZ9pGKASP570h8BVBFn5UBDH2";
+
+        typeface= ResourcesCompat.getFont(getApplicationContext(),R.font.didact_gothic);
+        auth = FirebaseAuth.getInstance();
+        listener=new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user=firebaseAuth.getCurrentUser();
+                if(user==null)
+                {
+                    Intent i=new Intent(Descriptive_type_result.this, Login_Signup_Activity.class);
+                    startActivity(i);
+                }
+
+            }
+        };
+    }
+    @Override
+    protected void onStart() {
+        super.onStart();
+        auth.addAuthStateListener(listener);
+
+    }
+}
