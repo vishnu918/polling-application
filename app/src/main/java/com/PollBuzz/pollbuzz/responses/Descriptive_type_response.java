9fe4b9bd0a1ff0d90a0e22edc2b48bdcf4705ef8
medhavi <medhavi.srivastava16@gmail.com>
2020-03-24 22:30:53 +0530

    [Medhavi] | Descriptive_Type_Response: Added to manifest after resolving

--- app/src/main/java/com/PollBuzz/pollbuzz/responses/Descriptive_type_response.java
+++ app/src/main/java/com/PollBuzz/pollbuzz/responses/Descriptive_type_response.java
@@ -1,4 +1,4 @@
-package com.PollBuzz.pollbuzz;
+package com.PollBuzz.pollbuzz.responses;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
@@ -14,13 +14,13 @@ import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
-import android.widget.CheckBox;
 import android.widget.ImageButton;
-import android.widget.LinearLayout;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.PollBuzz.pollbuzz.LogIn_SignUp.Login_Signup_Activity;
+import com.PollBuzz.pollbuzz.MainActivity;
+import com.PollBuzz.pollbuzz.Polldetails;
+import com.PollBuzz.pollbuzz.R;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputLayout;
@@ -59,7 +59,7 @@ public class Descriptive_type_response extends AppCompatActivity {
         View view =getSupportActionBar().getCustomView();
         home = view.findViewById(R.id.home);
         logout = view.findViewById(R.id.logout);
-        key="9redIYmBU023WNUNaWyc";
+        key="npZZ7n4OXlchpJsen8eJ";
         response=new HashMap<>();
         home.setOnClickListener(new View.OnClickListener() {
             @Override
