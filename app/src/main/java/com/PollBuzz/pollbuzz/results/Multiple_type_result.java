edb6b833adadb5aae037e676c536c28a2d285953
medhavi <medhavi.srivastava16@gmail.com>
2020-03-25 02:32:26 +0530

    [Medhavi] | Single_Type_Result: Added retrieving poll and answers for a voter

--- app/src/main/java/com/PollBuzz/pollbuzz/results/Multiple_type_result.java
+++ app/src/main/java/com/PollBuzz/pollbuzz/results/Multiple_type_result.java
@@ -7,6 +7,7 @@ import androidx.core.content.res.ResourcesCompat;
 
 import android.app.Dialog;
 import android.content.Intent;
+import android.graphics.Paint;
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.view.View;
@@ -15,14 +16,20 @@ import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 
 import com.PollBuzz.pollbuzz.LogIn_SignUp.Login_Signup_Activity;
 import com.PollBuzz.pollbuzz.MainActivity;
+import com.PollBuzz.pollbuzz.Polldetails;
 import com.PollBuzz.pollbuzz.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.HashMap;
@@ -40,14 +47,12 @@ public class Multiple_type_result extends AppCompatActivity {
     FirebaseAuth auth;
     ImageButton home,logout;
     FirebaseAuth.AuthStateListener listener;
-    Button submit;
-    Map<String,Integer> response;
+    Map<String,Object> response;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_multiple_type_result);
-        setContentView(R.layout.activity_single_type_result);
         getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
         getSupportActionBar().setDisplayShowCustomEnabled(true);
         getSupportActionBar().setCustomView(R.layout.action_bar);
@@ -73,7 +78,7 @@ public class Multiple_type_result extends AppCompatActivity {
         db=FirebaseFirestore.getInstance();
         options=new HashMap<>();
         response=new HashMap<>();
-        key= "XPkv84bvWMRXX4mEM97j";
+        key= "IydixH3XGmrNDnHwxTC7";
         uid="3fpFZ9pGKASP570h8BVBFn5UBDH2";
 
         typeface= ResourcesCompat.getFont(getApplicationContext(),R.font.didact_gothic);
@@ -92,6 +97,48 @@ public class Multiple_type_result extends AppCompatActivity {
 
             }
         };
+        db.collection("Polls").document(key).get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                                                                             @Override
+                                                                             public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+
+
+
+                                                                                 if (task.isSuccessful()) {
+
+                                                                                     DocumentSnapshot data = task.getResult();
+                                                                                     if(data.exists())
+                                                                                     {   group.removeAllViews();
+                                                                                         dialog.dismiss();
+                                                                                         Polldetails polldetails=data.toObject(Polldetails.class);
+                                                                                         title.setText(polldetails.getTitle());
+                                                                                         title.setPaintFlags(title.getPaintFlags()| Paint.UNDERLINE_TEXT_FLAG);
+                                                                                         query.setText(polldetails.getQuestion());
+                                                                                         options=polldetails.getMap();
+                                                                                         db.collection("Polls").document(key).collection("Response").document(uid).get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                                                                                             @Override
+                                                                                             public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                                                                                                 if(task.isSuccessful())
+                                                                                                 {
+                                                                                                     DocumentSnapshot data = task.getResult();
+                                                                                                     if(data.exists())
+                                                                                                     {
+                                                                                                         response=data.getData();
+                                                                                                         setOptions();
+                                                                                                     }
+                                                                                                 }
+
+                                                                                             }
+                                                                                         });
+
+
+
+                                                                                     }
+                                                                                 }
+
+                                                                             }
+                                                                         }
+        );
+
     }
     @Override
     protected void onStart() {
@@ -113,5 +160,49 @@ public class Multiple_type_result extends AppCompatActivity {
         dialog.setCancelable(false);
         dialog.show();
         window.setAttributes(lp);
+    }
+    private void setOptions()
+    {
+
+        for(Map.Entry<String,Object> entry: response.entrySet())
+        {
+            String key=entry.getKey();
+            if(options.containsKey(key))
+            {
+                options.remove(key);
+                options.put(key,1);
+            }
+        }
+        for(Map.Entry<String,Integer> entry : options.entrySet())
+        {
+            RadioButton button=new RadioButton(getApplicationContext());
+            LinearLayout.LayoutParams layoutParams=new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT,LinearLayout.LayoutParams.WRAP_CONTENT);
+            layoutParams.setMargins(5,20,5,20);
+            button.setLayoutParams(layoutParams);
+            button.setTypeface(typeface);
+            button.setText(entry.getKey());
+            button.setTextSize(20.0f);
+            group.addView(button);
+            if(entry.getValue()==1)
+                button.setChecked(true);
+            else
+                button.setEnabled(false);
+            button.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    RadioButton b=(RadioButton)v;
+                    if(entry.getValue()==1)
+                    {
+                        b.setChecked(true);
+                        if(b.isChecked()==false)
+                            b.setChecked(true);
+                    }
+
+                    else
+                        b.setChecked(false);
+                }
+            });
+        }
+        dialog.dismiss();
     }
 }
