7573e47ec0761cb5a9f6435c25c0167eae0e0da3
divyank00 <lunkaddivyank@gmail.com>
2020-04-01 20:51:18 +0530

    NAV Bar removed

--- app/src/main/java/com/PollBuzz/pollbuzz/MainActivity.java
+++ app/src/main/java/com/PollBuzz/pollbuzz/MainActivity.java
@@ -2,7 +2,6 @@ package com.PollBuzz.pollbuzz;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
@@ -16,18 +15,12 @@ import com.daimajia.androidanimations.library.YoYo;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.firebase.crashlytics.FirebaseCrashlytics;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import me.ibrahimsn.lib.SmoothBottomBar;
 
 public class MainActivity extends AppCompatActivity {
     public static SmoothBottomBar bottomBar;
-    public static Fragment active;
-    public static Fragment fragment1, fragment2, fragment3;
     private FragmentManager fm;
     private FloatingActionButton fab;
-    private List<Fragment> fList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,13 +39,8 @@ public class MainActivity extends AppCompatActivity {
         } catch (Exception e) {
             FirebaseCrashlytics.getInstance().log(e.getMessage());
         }
-        fragment1 = new HomeFeed();
-        fragment2 = new VotedFeed();
-        fragment3 = new ProfileFeed();
-        fList = new ArrayList<>();
         fm = getSupportFragmentManager();
-        newFragment(fragment1, "0");
-        fList.add(fragment1);
+        newFragment(new HomeFeed(), "0");
     }
 
     private void setListeners() {
@@ -66,19 +54,13 @@ public class MainActivity extends AppCompatActivity {
         bottomBar.setOnItemSelectedListener(i -> {
             switch (i) {
                 case 0:
-                    newFragment(fragment1, "0");
-                    fList.remove(fragment1);
-                    fList.add(fragment1);
+                    newFragment(new HomeFeed(), "0");
                     break;
                 case 1:
-                    newFragment(fragment2, "1");
-                    fList.remove(fragment2);
-                    fList.add(fragment2);
+                    newFragment(new VotedFeed(), "1");
                     break;
                 case 2:
-                    newFragment(fragment3, "2");
-                    fList.remove(fragment3);
-                    fList.add(fragment3);
+                    newFragment(new ProfileFeed(), "2");
                     break;
             }
         });
@@ -86,16 +68,10 @@ public class MainActivity extends AppCompatActivity {
 
     private void newFragment(Fragment fragment, String id) {
         try {
-            if (fList.contains(fragment)) {
-                Log.d("fragment", fragment.getTag() + " " + active.getTag());
-                fm.beginTransaction().show(fragment).hide(active).commit();
-            } else {
-                if (fm.getFragments().contains(fragment))
-                    fm.beginTransaction().show(fragment).hide(active).commit();
-                else
-                    fm.beginTransaction().add(R.id.container, fragment, id).commit();
-            }
-            active = fragment;
+            fm.beginTransaction()
+                    .setCustomAnimations(android.R.animator.fade_in, android.R.animator.fade_out)
+                    .add(R.id.container, fragment, id)
+                    .commit();
         } catch (Exception e) {
             FirebaseCrashlytics.getInstance().log(e.getMessage());
         }
@@ -103,31 +79,6 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public void onBackPressed() {
-        if (fList.size() == 1) {
-            if (fList.get(0) == fragment1)
-                super.onBackPressed();
-            else {
-                fList.clear();
-                newFragment(fragment1, "0");
-                fList.add(fragment1);
-            }
-        } else {
-            fList.remove(active);
-            fm.beginTransaction().remove(active).show(fList.get(fList.size() - 1)).commit();
-            active = fList.get(fList.size() - 1);
-        }
-        if (active.getTag() != null) {
-            switch (active.getTag()) {
-                case "0":
-                    bottomBar.setActiveItem(0);
-                    break;
-                case "1":
-                    bottomBar.setActiveItem(1);
-                    break;
-                case "2":
-                    bottomBar.setActiveItem(2);
-                    break;
-            }
-        }
+        super.onBackPressed();
     }
 }
